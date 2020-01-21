c47c8a6126ccb45e8ca72e2b476f46dedca47ce4
Disa de Beau <Disan@Disas-MacBook-Air.local>
2020-01-21 21:47:14 +0100

    finished codealong

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import styled from 'styled-components/macro'
+import { Card } from 'lib/Card'
+
+const ThinnerCard = styled(Card)`
+width: 400px; 
+`
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <ThinnerCard
+        coverImage="https://www.fillmurray.com/500/300"
+        thumbnailUrl="https://www.fillmurray.com/100/100"
+        title="This is the title"
+        secondaryText="Secondary title">
+        Child content here
+          </ThinnerCard>
+
+      <Card title="Only a title" secondaryText="text" />
+      <Card>
+        Hello
+      </Card>
     </div>
   )
 }
